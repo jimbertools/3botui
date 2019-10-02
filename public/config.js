f1ea9609c019413b4bb7e03e182d80d124b7816d
coeneivan <ivan@ivancoene.be>
2019-10-02 16:00:46 +0200

    Fixing urls

--- public/config.js
+++ public/config.js
@@ -1,7 +1,8 @@
 export default ({
   tfApiUrl: 'https://radar.threefold.io/api/',
-  jsApiUrl: 'http://localhost/actors/',
-  apiUrl: 'http://192.168.2.62:3000',
-  janus: 'http://192.168.2.62:8088/janus',
-  janusHTTPS: 'https://192.168.2.62:8089/janus'
+  jsApiUrl: `http://${window.location}/actors/`,
+
+  apiUrl: 'https://ffc-signaling.jimber.org',
+  janus: 'http://ffc-video.jimber.org/janus',
+  janusHTTPS: 'https://ffc-video.jimber.org/janus'
 })
